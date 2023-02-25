1a9dc8da449ec614fa464ffa93aeee76bbf8dfbe
BongBang <luis.cordova.olavarria>
2023-02-25 15:36:21 -0300

    termine la parte del newsletter y el comming soon

--- app.js
+++ app.js
@@ -12,25 +12,21 @@ var swiper = new Swiper(".home", {
       
     });
 
-    var swiper = new Swiper(".comming-container", {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      },
-    });
+var swiper = new Swiper(".comming-container", {
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
