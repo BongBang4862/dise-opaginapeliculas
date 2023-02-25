993e2d624c1dd3f1ba3895c67bb7be5662f60651
BongBang <luis.cordova.olavarria>
2023-02-25 14:09:11 -0300

    avance con comming soon y movies container

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ var swiper = new Swiper(".home", {
       },
       
     });
+
+    var swiper = new Swiper(".comming-container", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+    });
